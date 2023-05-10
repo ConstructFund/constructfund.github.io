52f880aeaacf7182e21c8aea4e6c7dffb5b56e35
Ossama <skymen75019@gmail.com>
2023-05-11 01:20:58 +0200

    First commit of the website

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,38 @@
 <template>
-  <v-app>
+  <v-app dark>
     <v-main>
-      <HelloWorld />
+      <v-toolbar app>
+        <v-toolbar-title>Construct Fund</v-toolbar-title>
+        <v-spacer></v-spacer>
+        <v-btn text @click="reportIssue()">
+          <v-icon right>mdi-open-in-new</v-icon>
+          Report issue
+        </v-btn>
+      </v-toolbar>
+      <div class="parent">
+        <Card v-for="(item, i) in content" key="i" :content="item" />
+      </div>
     </v-main>
   </v-app>
 </template>
 
 <script setup>
-  import HelloWorld from '@/components/HelloWorld.vue'
+import Card from "@/components/Card.vue";
+import content from "@/assets/content.json";
+
+function reportIssue() {
+  window.open("https://github.com/ConstructFund/color-addon-generator/issues/");
+}
 </script>
+
+<style scoped>
+.parent {
+  width: 100%;
+  height: calc(100% - 64px);
+  overflow: auto;
+  padding-left: 30%;
+  padding-right: 30%;
+  padding-top: 50px;
+  padding-bottom: 50px;
+}
+</style>
---
+++ src/components/Card.vue
@@ -0,0 +1,80 @@
+<template>
+  <v-card class="child">
+    <v-toolbar
+      style="padding-left: 15px; padding-right: 15px"
+      :color="content.hasWebsite ? 'primary' : ''"
+    >
+      <h2>{{ content.name }}</h2>
+      <v-spacer></v-spacer>
+      <v-btn @click="openWebsite" text v-if="content.hasWebsite">
+        Open Website
+      </v-btn>
+      <v-icon right>mdi-open-in-new</v-icon>
+    </v-toolbar>
+    <v-card-text>
+      {{ content.description }}
+    </v-card-text>
+    <v-card-actions>
+      <div>
+        <v-chip :color="chipColor">
+          <div class="prechip">Status:</div>
+          {{ content.status }}
+        </v-chip>
+      </div>
+      <div>
+        <v-btn @click="openRepo">Open Repo</v-btn>
+        <v-icon>mdi-github</v-icon>
+      </div>
+    </v-card-actions>
+  </v-card>
+</template>
+
+<script setup>
+import { computed } from "vue";
+
+// define prop for content object
+const props = defineProps({
+  content: Object,
+});
+
+const chipColor = computed(() => {
+  return {
+    "In Development": "success",
+    Maintenance: "warning",
+    "On Hiatus": "warning",
+    Abandonned: "error",
+    Shipped: "info",
+  }[props.content.status];
+});
+
+function openWebsite() {
+  window.open(props.content.url);
+}
+
+function openRepo() {
+  window.open(props.content.repo);
+}
+</script>
+
+<style scoped>
+.child {
+  margin-top: 20px;
+  margin-bottom: 20px;
+  width: 100%;
+}
+
+.prechip {
+  background-color: rgb(var(--v-theme-primary)) !important;
+  height: 100%;
+  margin-left: -12px;
+  text-align: center;
+  padding-left: 12px;
+  padding-right: 4px;
+  margin-right: 6px;
+  border-top-left-radius: 100px;
+  border-bottom-left-radius: 100px;
+  align-items: center;
+  display: flex;
+  color: white;
+}
+</style>
